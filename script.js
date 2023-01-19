66a36f55f86ff90027fe5a375c0b0af052cb1685
anasilva <anacmas@gmail.com>
2023-01-19 10:17:52 -0300

    feat: basic structure of js vars

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".button-count");
+const answer = document.querySelector(".answer");
+const inputContent = document.querySelector(".input-word").value;
+
+button.addEventListener("click", countVowels());
+
+function countVowels() {}
